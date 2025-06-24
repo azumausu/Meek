ad8c9e52989f42fa889b59a590c95ce05013ecda
h.amano <h.amano@amatech.jp>
2025-06-24 16:11:09 +0900

    [MOD] fix DefaultPrefabViewMamanger

--- Assets/Packages/Meek.UGUI/Runtime/DefaultPrefabViewManager.cs
+++ Assets/Packages/Meek.UGUI/Runtime/DefaultPrefabViewManager.cs
@@ -1,7 +1,4 @@
 #if MEEK_ENABLE_UGUI
-using System;
-using System.Linq;
-using Meek.NavigationStack;
 using UnityEngine;
 
 namespace Meek.UGUI
@@ -11,20 +8,6 @@ namespace Meek.UGUI
         [SerializeField] private RectTransform _rootNode;
 
         public Transform PrefabRootNode => _rootNode;
-
-
-        void IPrefabViewManager.SortOrderInHierarchy(NavigationContext navigationContext)
-        {
-            var navigationService = navigationContext.AppServices.GetService<StackNavigationService>();
-            var uis = navigationService.ScreenContainer.Screens.OfType<StackScreen>().Select(x => x.UI);
-            foreach (var ui in uis)
-            {
-                foreach (var prefabView in ui.ViewHandlers.Reverse().OfType<PrefabViewHandler>())
-                {
-                    prefabView.RootNode.SetAsFirstSibling();
-                }
-            }
-        }
     }
 }
 #endif
--- Assets/Packages/Meek.UGUI/Runtime/IPrefabViewManager.cs
+++ Assets/Packages/Meek.UGUI/Runtime/IPrefabViewManager.cs
@@ -1,13 +1,27 @@
+using Meek.NavigationStack;
 using UnityEngine;
+using System;
+using System.Linq;
 
 #if MEEK_ENABLE_UGUI
 namespace Meek.UGUI
 {
     public interface IPrefabViewManager
     {
-        public Transform PrefabRootNode { get; }
+        Transform PrefabRootNode { get; }
 
-        void SortOrderInHierarchy(NavigationContext context);
+        void SortOrderInHierarchy(NavigationContext navigationContext)
+        {
+            var navigationService = navigationContext.AppServices.GetService<StackNavigationService>();
+            var uis = navigationService.ScreenContainer.Screens.OfType<StackScreen>().Select(x => x.UI);
+            foreach (var ui in uis)
+            {
+                foreach (var prefabView in ui.ViewHandlers.Reverse().OfType<PrefabViewHandler>())
+                {
+                    prefabView.RootNode.SetAsFirstSibling();
+                }
+            }
+        }
     }
 }
 #endif
